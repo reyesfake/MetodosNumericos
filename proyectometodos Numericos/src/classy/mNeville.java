523e364ae56592f435946aab77dd89f03802c654
Jose Reyes <72475604+reyesfake@users.noreply.github.com>
2023-05-28 21:55:55 -0600

    Add files via upload

---
+++ proyectometodos Numericos/src/classy/mNeville.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classy;
+import interfaz.Neville4;
+
+/**
+ *
+ * @author ProcesosE
+ */
+
+
+public class mNeville {
+    
+    
+        
+   
+   
+
+ public void dosDatos(){
+     
+    
+    
+ }
+ public static void main(String[] args) {
+    
+        
+        
+    }
+    
+}
+
